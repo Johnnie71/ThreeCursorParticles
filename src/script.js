e0f85b2e3ed83963175a0052c167b889d40c5f4a
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-09-20 14:49:59 -0400

    added 2D canvas for testing

--- src/script.js
+++ src/script.js
@@ -66,6 +66,48 @@ renderer.setClearColor('#181818')
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
+// 2D Canvas
+const displacement = {}
+displacement.canvas = document.createElement('canvas')
+displacement.canvas.width = 128
+displacement.canvas.height = 128
+displacement.canvas.style.position = 'fixed'
+displacement.canvas.style.width = '256px'
+displacement.canvas.style.height = '256px'
+displacement.canvas.style.top = '0'
+displacement.canvas.style.left = '0'
+displacement.canvas.style.zIndex = '10'
+document.body.append(displacement.canvas)
+
+// Context
+displacement.context = displacement.canvas.getContext('2d')
+displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+
+// Glow image
+displacement.glowImage = new Image()
+displacement.glowImage.src = './glow.png'
+
+// Interactive plane
+displacement.interactivePlane = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshBasicMaterial({ color: 'red'})
+)
+scene.add(displacement.interactivePlane)
+
+// Raycaster
+displacement.raycaster = new THREE.Raycaster()
+
+// Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursor = new THREE.Vector2(9999, 9999)
+
+window.addEventListener('pointermove', (e) => {
+    const { clientX, clientY } = e
+    displacement.screenCursor.x = (clientX / sizes.width) * 2 - 1
+    displacement.screenCursor.y = - (clientY / sizes.height) * 2 + 1
+
+})
+
 /**
  * Particles
  */
@@ -90,6 +132,28 @@ const tick = () =>
 {
     // Update controls
     controls.update()
+
+    // Raycaster
+    displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+    const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane)
+
+    if (intersections.length) {
+        const uv = intersections[0].uv
+        displacement.canvasCursor.x = uv.x * displacement.canvas.width
+        displacement.canvasCursor.y = (1 - uv.y)* displacement.canvas.height
+    }
+
+    // Displacement
+    // Draw glow
+    const glowSize = displacement.canvas.width * 0.25
+    displacement.context.globalCompositeOperation = 'lighten'
+    displacement.context.drawImage(
+        displacement.glowImage,
+        displacement.canvasCursor.x - glowSize * 0.5,
+        displacement.canvasCursor.y - glowSize * 0.5,
+        glowSize,
+        glowSize
+    )
 
     // Render
     renderer.render(scene, camera)
